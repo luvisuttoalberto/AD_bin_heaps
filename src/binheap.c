fdb373389f36eb478550386a1610ba252063a76b
Alberto Luvisutto <luvisutto.alberto@gmail.com>
2021-01-12 11:46:08 +0100

    fixing comments

--- src/binheap.c
+++ src/binheap.c
@@ -22,7 +22,7 @@ const void *min_value(const binheap_type *H)
         return NULL;
     }
 
-    // the minimum is stored in the root a.k.a A[0]
+    // the minimum is stored in the root, a.k.a A[0]
     return ADDR(H, 0);
 }
 
@@ -44,8 +44,8 @@ void heapify(binheap_type * H, unsigned int node)
 {
     unsigned int dst_node=node, child;
     do {
-        //this is needed for all the successive iteration (apart from the first one)
-        // useless for the first iteration
+        // this is needed for all the successive iteration, first one excluded
+        // it's useless for the first iteration
         node = dst_node;
 
         // find the minimum among node and its children
@@ -75,13 +75,14 @@ const void *extract_min(binheap_type *H)
         return NULL;
     }
 
-    // swapping the keys among the root anmong the root (A[0])
+    // swapping the keys of the root (A[0])
     // and the right-most leaf of the
     // last level (A[num_of_elem-1])
     swap_keys(H, 0, H->num_of_elem-1);
 
     // deleting the right-most leaf of the
     // last level (A[num_of_elem-1])
+    // which previously was the root
     H->num_of_elem--;
 
     heapify(H, 0);
@@ -100,10 +101,10 @@ const void * find_the_max(void *A,
 
 	const void *max_value = A;
 
-	//for all the values in A
-	for(const void *addr=A+key_size; addr!=A+num_of_elem*key_size; addr +=key_size){
+	// for all the values in A
+	for(const void *addr = A + key_size; addr != A + num_of_elem * key_size; addr += key_size){
 
-		//if addr > max_value
+		// if addr > max_value
 		if(!leq(addr, max_value)){
 			max_value = addr;
 		}
@@ -126,21 +127,22 @@ binheap_type *build_heap(void *A,
 	H->max_size = max_size;
 	H->key_size = key_size;
 	H->leq = leq;
-	H->max_order_value = malloc(key_size); // not typecasting the malloc since max_order_value is a pointer to void
-	// I allocated the memory for this previous value because 
-	// the old max_order_value might not be available anymore
+	H->max_order_value = malloc(key_size); 
+    // in the previous line we are not typecasting the malloc,
+    // since max_order_value is a pointer to void.
+	// We perform an allocation because the "old" max_order_value
+    // might not be available anymore
 
 	if(num_of_elem == 0){
 		return H;
 	}
 
-	// get the maximum among A[:num_of_elem-1]
+	// get the maximum among A[:num_of_elem-1] (all the elements)
 	// and store it in max_order_value
 	const void * value = find_the_max(A, num_of_elem, key_size, leq);
 	memcpy(H->max_order_value, value, key_size);
 
-	// fix the heap property from the second last level
-	// up to the root
+	// fix the heap property from the second last level to the root
 	for(unsigned int i = num_of_elem/2; i > 0; i--){
 		heapify(H, i);
 	}
@@ -190,18 +192,22 @@ const void *decrease_key(binheap_type *H, void *node, const void *value)
 
 const void *insert_value(binheap_type *H, const void *value)
 {
-    //if the heap is already full
+    // if the heap is already full
+    // return NULL
     if(H->max_size == H->num_of_elem){
     	return NULL;
     }
 
     // if the new value > *max_order_value
+    // change the max_order_value
     if(H->num_of_elem == 0 || !H->leq(value, H->max_order_value)){
     	memcpy(H->max_order_value, value, H->key_size);
     }
 
     // get the position of the new node
     void *new_node_addr = ADDR(H, H->num_of_elem);
+
+    // assign to the new node the max_order_value
     memcpy(new_node_addr, H->max_order_value, H->key_size);
     
     // increase the size of the heap
@@ -214,9 +220,10 @@ const void *insert_value(binheap_type *H, const void *value)
 void print_heap(const binheap_type *H, 
                 void (*key_printer)(const void *value))
 {
-    unsigned int next_level_node = 1;   // stores the index of the 
-                                        // left-most node of the
-                                        // next level
+
+    // stores the index of the left-most node of the next level
+    unsigned int next_level_node = 1;
+    
     for (unsigned int node = 0; node < H->num_of_elem; node++){
         if (node == next_level_node){
             printf("\n");
